9473851f17017a0bf8b29d033dbda5804ae1dda3
VoiceShifter <golosov-den@mail.ru>
2023-10-09 14:03:32 +0300

    feat: added functions

---
+++ Functions.cpp
@@ -0,0 +1,58 @@
+# include <Functions.hpp>
+void EncryptStringCesarus (string &ToEncrypt, signed int Key)
+{
+
+    	for (char &Character : ToEncrypt)
+      	{
+		if ((int(Character)+Key) > 122)
+		{
+			unsigned int Offset{int(Character)+Key-122};
+			Character=char(97 + Offset)
+		}
+		else
+		{
+			Character += Key;
+		}
+      	}
+}
+
+void EncryptASCII(string &ToEncrypt, signed int Key)
+{
+	for (char &Character : ToEncrypt)
+	{
+		Character += Key;
+	}
+}
+
+void EncryptStringVigener(string &ToEncrypt, signed int Key)
+{
+	return;
+}
+
+void GetFile(string FileName)
+{
+	std::ifstream File (FileName, std::ios_base::out | std::ios_base::app);
+        if (!File.is_open())
+        {
+                std::cout << "Error opening original file\n";
+                return;
+        }
+        //std::cout << File.rdbuf() << '\n';
+        FileName = 'e' + FileName;
+        std::fstream eFile (FileName, std::ios_base::app | std::ios_base::in | std::ios_base::out);
+        if (eFile.is_open())
+        {
+                //std::cout << "Sucksess\n";
+        }
+        string Buffer{};
+        while(getline(File, Buffer))
+        {
+                std::cout << Buffer << '\n';
+                EncryptStringCesarus(Buffer, 1);
+                eFile << Buffer;
+        }
+        eFile.close();
+	File.close();
+
+}
+
---
+++ Functions.hpp
@@ -0,0 +1,15 @@
+# include <iostream>
+# include <fstream>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string>
+
+
+using std::string;
+void EncryptStringCesarus (string &, signed int);
+void EncryptStringVigener (string &, signed int);
+void EncryptASCII(string &, signed int);
+void GetFile(string);
+std::string exec(const char* cmd);
+
+
