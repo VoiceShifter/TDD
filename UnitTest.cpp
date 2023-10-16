40b54d4c2b198f4ebd892fce0801008c8b837252
VoiceShifter <golosov-den@mail.ru>
2023-10-16 13:16:55 +0300

    feat: Finale UnitTest

--- UnitTest.cpp
+++ UnitTest.cpp
@@ -1,8 +1,50 @@
-# include <>
 # include <assert.h>
+# include <iostream>
+# include <string>
+
+
+using std::string;
+
+void EncryptStringCesarus (string &ToEncrypt, signed int Key)
+{
+
+        for (char &Character : ToEncrypt)
+        {
+                if ((int(Character)+Key) > 122)
+                {
+                        unsigned int Offset{int(Character)+Key-123};
+                        Character=char(97 + Offset);
+                }
+                else
+                {
+                        Character += Key;
+                }
+        }
+};
+
+void EncryptASCII(string &ToEncrypt, signed int Key)
+{
+        for (char &Character : ToEncrypt)
+        {
+                Character += Key;
+        }
+};
+
 void Tests()
 {
-	assert(EncryptStringCesarus("abc", 1) == "bcd");
-	//assert(EncryptStringVigener("abc", 1) == "bdf");
-	assert(EncryptASCII("abc", 10) == "klm");
+	std::string Test{"xyz"};
+	EncryptStringCesarus(Test, 1);
+	std::cout << Test << '\n';
+        assert(Test== "yza");
+        //assert(EncryptStringVigener("abc", 1) == "bdf");
+	Test="xyz";
+        EncryptASCII(Test, 1);
+	std::cout << Test << '\n';
+	assert(Test== "yz{");
+};
+
+signed int main()
+{
+	Tests();
+	return 0;	
 }
